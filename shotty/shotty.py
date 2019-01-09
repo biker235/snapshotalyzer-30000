2dcf981b9b114160796382c7e04857382ea734b5
U-50E65309CTJLX1\wholzapfel <wholzapfel@50E65309CTJLX1.(none)>
2019-01-09 10:53:24 -0600

    Add options start and stop to shotty/shotty.py

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,6 +4,18 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+
 @click.group()
 def instances():
     """Commmands for instances"""
@@ -14,13 +26,8 @@ def instances():
 
 def list_instances(project):
     "List EC2 instances"
-    instances =[]
 
-    if project:
-        filters = [{'Name':'tag:Project', 'Values':[project]}]
-        instances = ec2.instances.filter(Filters=filters)
-    else:
-        instances = ec2.instances.all()
+    instances = filter_instances(project)
 
     for i in instances:
         tags = { t['Key']: t['Value'] for t in i.tags or []}
@@ -40,16 +47,25 @@ def list_instances(project):
 def stop_instances(project):
     "Stop EC2 instances"
 
-    instances = []
-    if project:
-        filters = [{'Name':'tag:Project', 'Values':[project]}]
-        instances = ec2.instances.filter(Filters=filters)
-    else:
-        instances = ec2.instances.all()
+    instances = filter_instances(project)
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
         i.stop()
+
+    return
+
+@instances.command('start')
+@click.option('--project', default=None,
+    help='Only instances for project')
+def start_instances(project):
+    "Starting EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
 
     return
 
